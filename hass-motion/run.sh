1cafbaa4a8416a7caaa06983745f0b3a5bbc6b1a
fliaping <xuping@dianwoda.com>
2018-09-25 21:40:28 +0800

    修复设备名字变更

--- hass-motion/run.sh
+++ hass-motion/run.sh
@@ -4,6 +4,10 @@ CONFIG_PATH=/data/options.json
 TIMEZONE=$(jq --raw-output ".timezone" $CONFIG_PATH)
 
 date && export TZ=":/usr/share/zoneinfo/"$TIMEZONE && date
+
+ls /dev/
+mount /dev/disk/by-uuid/34cf2532-ddc5-4228-8d0c-a737503c3915 /share/disk/data
+
 mkdir -p /config/motioneye/
 test -e /config/motioneye/motioneye.conf || \
 cp /usr/share/motioneye/extra/motioneye.conf.sample /config/motioneye/motioneye.conf
