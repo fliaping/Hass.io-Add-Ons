99bce25fa6f3c936c2613a1bbbc8262d4b895ebb
fliaping <xuping@dianwoda.com>
2018-09-25 15:51:20 +0800

    motion timezone

--- hass-motion/run.sh
+++ hass-motion/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+CONFIG_PATH=/data/options.json
+
+TIMEZONE=$(jq --raw-output ".timezone" $CONFIG_PATH)
+
+date && export TZ=":/usr/share/zoneinfo/"$TIMEZONE && date
 mkdir -p /config/motioneye/
 test -e /config/motioneye/motioneye.conf || \
 cp /usr/share/motioneye/extra/motioneye.conf.sample /config/motioneye/motioneye.conf
