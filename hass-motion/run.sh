8438d60eb305e14dd49055c13d97aae7fb429b58
fliaping <xuping19940812@gmail.com>
2018-09-25 22:45:47 +0800

    mount

--- hass-motion/run.sh
+++ hass-motion/run.sh
@@ -6,7 +6,7 @@ TIMEZONE=$(jq --raw-output ".timezone" $CONFIG_PATH)
 date && export TZ=":/usr/share/zoneinfo/"$TIMEZONE && date
 
 ls /dev/
-mount /dev/disk/by-uuid/34cf2532-ddc5-4228-8d0c-a737503c3915 /share/disk/data
+mount /dev/sda1 /share/disk/data
 
 mkdir -p /config/motioneye/
 test -e /config/motioneye/motioneye.conf || \
